bbb38e7f64c68666547ec05012849435d950e171
Felix Godziela <49444169+jose-godziela@users.noreply.github.com>
2020-02-01 12:00:42 -0300

    Added credits scene; missing names

--- Game Jamp 2020/Assets/Scripts/UI/Menu.cs
+++ Game Jamp 2020/Assets/Scripts/UI/Menu.cs
@@ -9,20 +9,26 @@ public class Menu : MonoBehaviour
     public void start_game()
     {
         Debug.Log("Empeze el juego");
-        cargar_juego("Game_screen");
-        
+        cargar_pantalla("Game_screen");
+
     }
     public void credits_screen()
     {
         Debug.Log("Mira quien carajo hizo el juego");
+        cargar_pantalla("FG_Credits_Screen");
     }
     public void exit_game()
     {
         Debug.Log("Me rajo de esta mierda");
-        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+      Application.Quit();
+#endif
+
     }
 
-    private void cargar_juego(string juego)
+    private void cargar_pantalla(string juego)
     {
         SceneManager.LoadScene(juego);
     }
