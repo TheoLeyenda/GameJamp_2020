39c9503fbfb30ebd4ee1f771581d06bcb39b7be6
Federico Coppola <fgcoppola92@gmail.com>
2020-02-01 12:59:44 -0300

    plataforma movible

---
+++ Game Jamp 2020/Assets/Scripts/background/Platform.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : PlatformMovement
+{
+    private Rigidbody2D rig2D;
+    public PlatformMovement PlatformMovement;
+    private GameObject player;
+    private GameObject platformChild;
+    private float actualTime;
+
+    [Header("Tipo de movimiento")]
+    public PlatformMovement movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        platformChild = GameObject.FindWithTag("Floor");
+        actualTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(actualTime> loopTimeMovement)
+        {
+            actualTime++;
+            movePlatformLateralLeft();
+        }
+        else
+        {
+            actualTime++;
+            movePlatformLateralRight();
+        };
+        if (actualTime > (loopTimeMovement*2))
+        {
+            actualTime = 0;
+        }
+        
+
+    }
+
+    void movePlatformLateralRight()
+    {
+        platformChild.transform.position = platformChild.transform.position + new Vector3(speedMovement, 0, 0) * Time.deltaTime;
+    }
+
+    void movePlatformLateralLeft()
+    {
+        platformChild.transform.position = platformChild.transform.position - new Vector3(speedMovement, 0, 0) * Time.deltaTime;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Entro");
+            player.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit2d(Collision2D collision)
+    {
+        Debug.Log("Salio");
+        player.transform.parent = null;
+    }
+}
+
+
---
+++ Game Jamp 2020/Assets/Scripts/background/PlatformMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public float speedMovement;
+    public float direction;
+    protected bool inMovement;
+    public float loopTimeMovement;
+   
+}
