b0e465df30d53aa86f90146a26d3aee020c9badb
Felix Godziela <49444169+jose-godziela@users.noreply.github.com>
2020-02-01 10:54:07 -0300

    Añadido base menu principal

---
+++ Game Jamp 2020/Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+   /* public int damage;
+    public Characther characther;
+    public void DamageCharacter()
+    {
+        characther.life = characther.life - damage;
+    }*/
+
+
+}
