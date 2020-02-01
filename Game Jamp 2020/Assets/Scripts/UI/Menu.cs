53ee0eb18bc75c955281b3e5874be3bf1725b5c5
Felix Godziela <49444169+jose-godziela@users.noreply.github.com>
2020-02-01 11:34:55 -0300

    Updated menu: now it changes to the game

--- Game Jamp 2020/Assets/Scripts/UI/Menu.cs
+++ Game Jamp 2020/Assets/Scripts/UI/Menu.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-   /* public int damage;
-    public Characther characther;
-    public void DamageCharacter()
-    {
-        characther.life = characther.life - damage;
-    }*/
 
+    public void start_game()
+    {
+        Debug.Log("Empeze el juego");
+        cargar_juego("Game_screen");
+        
+    }
+    public void credits_screen()
+    {
+        Debug.Log("Mira quien carajo hizo el juego");
+    }
+    public void exit_game()
+    {
+        Debug.Log("Me rajo de esta mierda");
+        
+    }
+
+    private void cargar_juego(string juego)
+    {
+        SceneManager.LoadScene(juego);
+    }
 
 }
